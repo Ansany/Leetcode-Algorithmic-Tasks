de00620ccccdbcac084d17c1be01b5a6029ecd09
Andrey Alymov <68674699+Ansany@users.noreply.github.com>
2022-02-22 18:54:55 +0300

    Create BestTimeToBuySol.swift

---
+++ Leetcode-Algoritmic-Tasks/EasyLevel/BestTimeToBuySolution/BestTimeToBuySol.swift
@@ -0,0 +1,16 @@
+    func maxProfit(_ prices: [Int]) -> Int {
+        var buy: Int?
+        var profit = 0
+        for i in prices {
+            if let buyVal = buy {
+                let currentProfit = i - buyVal
+                profit = max(currentProfit, profit)
+                if currentProfit < 0 {
+                    buy = i
+                }
+            } else {
+                buy = i
+            }
+        }
+        return profit
+    }
